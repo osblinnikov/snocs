38e806d43596ebb79f63febde56b0fd3019a9c48
Oleg Blinnikov <osblinnikov@gmail.com>
2015-06-15 17:01:57 +0200

    removed unnecessary restriction

--- gcc.py
+++ gcc.py
@@ -8,12 +8,12 @@ def prepare_gcc(env):
     env['CPPPATH'].extend([])
     env['CPPDEFINES'].extend([])
     if env['PLATFORM'] == 'x86':
-        env['CCFLAGS'].extend(['-m32','-fpic','-Wall','-Werror'])
+        env['CCFLAGS'].extend(['-m32','-fpic','-Werror'])
         env['LINKFLAGS'].extend(['-m32'])
         env['LIBS'].extend([])#'stdc++'
         env['LIBPATH'].extend(['/usr/lib32'])
     elif env['PLATFORM'] == 'x64':
-        env['CCFLAGS'].extend(['-m64','-fpic','-Wall','-Werror'])
+        env['CCFLAGS'].extend(['-m64','-fpic','-Werror'])
         env['LINKFLAGS'].extend(['-m64'])
         env['LIBPATH'].extend(['/usr/local/lib64'])
     else:
